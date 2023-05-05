5b735a30074e59df8bbb5f7d048bb56b2582335c
Alix Fullerton <fullertonalix0@gmail.com>
2023-05-05 12:58:04 -0400

    Adding PR deploy-test workflow

---
+++ tests/regression/api/test_ctrl_index.py
@@ -0,0 +1,39 @@
+"""
+    Cver - Test - Regression
+    CTRL Index
+        Checks that all routes on / are working properly.
+
+"""
+
+import os
+import requests
+
+CVER_API_URL = os.environ.get("CVER_API_URL")
+CVER_API_CLIENT_ID = os.environ.get("CVER_API_CLIENT_ID")
+CVER_API_KEY = os.environ.get("CVER_API_KEY")
+HEADERS = {
+    "client-id": CVER_API_CLIENT_ID,
+    "x-api-key": CVER_API_KEY
+}
+
+URL_BASE = "/"
+
+
+class TestApiIndex:
+
+    def test__index_get(self):
+        """Tests the Cver index through the Cver Api
+        GET /
+        """
+        request_args = {
+            "headers": HEADERS,
+            "method": "GET",
+            "url": "%s%s" % (CVER_API_URL, URL_BASE),
+        }
+
+        response = requests.request(**request_args)
+        assert response.status_code == 200
+        response_json = response.json()
+        assert "version" in response_json
+
+# End File: cver/tests/regression/api/index.py
