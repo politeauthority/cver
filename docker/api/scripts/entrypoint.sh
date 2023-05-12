bd36a91e1bbeb9729a728ade3249b6ccce4dc25c
Alix Fullerton <fullertonalix0@gmail.com>
2023-05-12 13:06:58 -0400

    Updating migration runner

---
+++ docker/api/scripts/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -l
+# Cver Api Entry Point
+set -e
+echo "Pignus Api Starting"
+python3 /app/cver/migrate/migrate.py
+echo "Migrations Completed"
+cd /app/cver/api
+gunicorn app:app --bind 0.0.0.0:5001
