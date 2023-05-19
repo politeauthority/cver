6268b000dc954ed23c6d6a1df5a813b71b5fcd35
Alix Fullerton <fullertonalix0@gmail.com>
2023-05-19 17:30:46 -0400

    Adding image-build unit tests

---
+++ tests/unit/shared/models/test_image_build.py
@@ -0,0 +1,27 @@
+"""
+    Cver Test Unit
+    Shared Model: ImageBuild
+
+"""
+
+from cver.shared.models.image_build import FIELD_MAP
+
+
+FIELD_DETAILS = ["name", "type", "primary", "default", "extra"]
+
+
+class TestSharedImageBuild:
+
+    def test__FIELD_MAP(self):
+        """Test the ImageBuild model's initialization.
+        :data: image_build.FIELD_MAP
+        """
+        assert isinstance(FIELD_MAP, dict)
+        for field_name, field in FIELD_MAP.items():
+            assert isinstance(field_name, str)
+            assert "name" in field
+            for field_detail, field_detail_value in field.items():
+                assert field_detail in FIELD_DETAILS
+
+
+# End File: cver/tests/unit/shared/models/test_image_build.py
