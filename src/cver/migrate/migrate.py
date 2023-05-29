5d31387facafdefa183a5a91c5228b848c6f8cf9
Alix Fullerton <fullertonalix0@gmail.com>
2023-05-29 12:48:25 -0400

    Updating migrate process

--- src/cver/migrate/migrate.py
+++ src/cver/migrate/migrate.py
@@ -191,7 +191,7 @@ class Migrate:
         print("\t%s" % user)
         print("\t%s" % api_key)
         print("\t Client ID: %s" % client_id)
-        print("\t Api Key: %s" % key)
+        print("\t Api Key: %s" % test_api_key)
 
         return True
 
