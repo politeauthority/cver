4b67937abcaa41736b9d685b5ba468a6dd772643
Alix Fullerton <fullertonalix0@gmail.com>
2023-05-29 15:08:07 -0400

    Move errors to JSON for the api

--- src/cver/api/app.py
+++ src/cver/api/app.py
@@ -9,6 +9,7 @@ from logging.config import dictConfig
 
 
 from flask import Flask
+from werkzeug.exceptions import HTTPException
 
 from cver.api.utils import db
 from cver.api.utils import glow
@@ -56,6 +57,19 @@ def register_blueprints(app: Flask) -> bool:
     app.register_blueprint(ctrl_software)
     app.register_blueprint(ctrl_softwares)
     return True
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 
 app = Flask(__name__)
