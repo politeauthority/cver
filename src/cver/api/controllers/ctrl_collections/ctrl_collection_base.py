3336907081d22cb1a34f699b8ef25bacd020b9ba
Alix Fullerton <fullertonalix0@gmail.com>
2023-09-19 22:02:03 -0400

    Adding collections tests

--- src/cver/api/controllers/ctrl_collections/ctrl_collection_base.py
+++ src/cver/api/controllers/ctrl_collections/ctrl_collection_base.py
@@ -16,16 +16,9 @@ def get(collection) -> dict:
         "object_type": _get_object_type(collection)
     }
     field_map = collection().collect_model().field_map
-    where_and = []
-    if request_args["raw_args"]:
-        for raw_arg_field, raw_arg_value in request_args["raw_args"].items():
-            if raw_arg_field in field_map and field_map[raw_arg_field]["api_searchable"]:
-                where = {
-                    "field": raw_arg_field,
-                    "value": raw_arg_value,
-                    "op": "="
-                }
-                where_and.append(where)
+    where_and = _get_where_and(request_args["raw_args"], field_map)
+
+    # Get the data
     collect_data = collection().get_paginated(where_and=where_and)
     for obj in collect_data["objects"]:
         data["objects"].append(obj.json())
@@ -44,7 +37,28 @@ def get(collection) -> dict:
     return data
 
 
+def _get_where_and(raw_args: dict, field_map: dict) -> list:
+    """Gets the where_and value to be sent to collection paginationation search from the raw_args
+    submitted in the requset
+    """
+    where_and = []
+    if not raw_args:
+        return []
+    for raw_arg_field, raw_arg_value in raw_args.items():
+        if raw_arg_field in field_map and field_map[raw_arg_field]["api_searchable"]:
+            where = {
+                "field": raw_arg_field,
+                "value": raw_arg_value,
+                "op": "="
+            }
+            where_and.append(where)
+    return where_and
+
+
 def _get_object_type(collection) -> str:
+    """Get an object name from the collection.
+    :unit-test: TestCtrlCollectionBase.test___get_object_type
+    """
     object_type = collection().collect_model.model_name
     if "_" in object_type:
         object_type = object_type.replace("_", "-")
