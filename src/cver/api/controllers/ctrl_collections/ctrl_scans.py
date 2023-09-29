bae6a46c50fddb591fa8025810855393fbe22ff7
Alix Fullerton <fullertonalix0@gmail.com>
2023-09-29 15:49:40 -0400

    Adding a few tests, fixing scan route

--- src/cver/api/controllers/ctrl_collections/ctrl_scans.py
+++ src/cver/api/controllers/ctrl_collections/ctrl_scans.py
@@ -7,7 +7,7 @@
 from flask import Blueprint, jsonify
 
 from cver.api.collects.scans import Scans
-from cver.api.utils import api_util
+from cver.api.controllers.ctrl_collections import ctrl_collection_base
 from cver.api.utils import auth
 
 ctrl_scans = Blueprint("scans", __name__, url_prefix="/scans")
@@ -17,9 +17,7 @@ ctrl_scans = Blueprint("scans", __name__, url_prefix="/scans")
 @ctrl_scans.route("/")
 @auth.auth_request
 def index():
-    args = api_util.get_params()
-    data = Scans().get_paginated(**args)
-    data["info"]["object_type"] = "scans"
+    data = ctrl_collection_base.get(Scans)
     return jsonify(data)
 
 
---
+++ tests/regression/cver_client/collections/test_image_build_waitings.py
@@ -0,0 +1,20 @@
+"""
+    Cver Test Regression
+    CverClient - Collections - ImageBuildWaitings
+    Source: src/cver/cver_client/collections/image_build_waitings.py
+
+"""
+
+from cver.cver_client.collections.image_build_waitings import ImageBuildWaitings
+
+
+class TestCverClientCollectionsImageBuildWaitings:
+
+    def test__get(self):
+        """Test that we can login to the Cver Api and that we store the token locally in a temp
+        file.
+        """
+        ibws = ImageBuildWaitings().get()
+        assert isinstance(ibws, list)
+
+# End File: cver/tests/regression/cver_client/collections/test_image_build_waitings.py
