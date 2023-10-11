7b3dc1498d53d74770f66287d9bec2a87074f108
Alix Fullerton <fullertonalix0@gmail.com>
2023-10-11 10:09:17 -0400

    Fixing pep8

--- src/cver/shared/utils/date_utils.py
+++ src/cver/shared/utils/date_utils.py
@@ -38,7 +38,7 @@ def json_date_out(the_datetime: datetime) -> str:
 
 
 def json_date_now() -> str:
-    """Get a JSON returnable value from now in UTC. Use this method for sending dates to the 
+    """Get a JSON returnable value from now in UTC. Use this method for sending dates to the
     Cver Api
     :unit-test: TestSharedUtilsDateUtils.test__json_date_now
     """
