a8c9a52ee65b9a9dd59753fa2030088e28fc915a
Alix Fullerton <fullertonalix0@gmail.com>
2023-10-11 09:24:24 -0400

    Fixing entrypoint

--- docker/engine/scripts/entrypoint.sh
+++ docker/engine/scripts/entrypoint.sh
@@ -2,4 +2,4 @@
 # Cver Engine Entrypoint
 set -e
 echo "Cver Engine Starting"
-/usr/bin/timeout ${TIMEOUT_SECONDS} python3 /app/cver/ingest/engine.py
+/usr/bin/timeout ${TIMEOUT_SECONDS} python3 /app/cver/engine/engine.py
