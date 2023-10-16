d85f0e0140b3213e0a9dc12778acdd31869400f7
Alix Fullerton <fullertonalix0@gmail.com>
2023-10-16 15:26:22 -0400

    prepping a working engine scan

--- src/cver/engine/modules/image_scan.py
+++ src/cver/engine/modules/image_scan.py
@@ -10,7 +10,6 @@ from cver.cver_client.models.image import Image
 from cver.cver_client.models.image_build import ImageBuild
 from cver.cver_client.models.task import Task
 from cver.cver_client.models.scan import Scan
-from cver.engine.utils import glow
 from cver.engine.utils import scan as scan_util
 
 
@@ -102,7 +101,6 @@ class ImageScan:
         """Gets the download location for the image and executes the download."""
         logging.info("Starting scan of: %s" % self.image.name)
         scan_result = scan_util.run_trivy(self.image, self.ib)
-<<<<<<< HEAD
         if not scan_result:
             logging.error("Scan failed for: %s" % self.image)
             self._handle_error_scan()
@@ -146,49 +144,6 @@ class ImageScan:
             return False
 
     def _handle_error_scan(self) -> bool:
-=======
-        import ipdb; ipdb.set_trace()
-
-        if not scan_result:
-            logging.error("Scan failed")
-            return False
-        logging.info("Successfully downloaded: %s" % self.image)
-        if not self.ib:
-            logging.info("lets create an ibw")
-            self._create_ib_from_pull(image_pull.decode("utf-8"))
-
-        self._handle_success_pull()
-        return True
-
-    def get_docker_pull_url(self) -> str:
-        """Get the appropriate image url to pull from, based on pull through registries that have
-        been preset.
-        """
-        if self.image.registry in glow.registry_info["pull_thrus"]:
-            image_loc = "%s/%s/%s:%s" % (
-                glow.registry_info["local"]["url"],
-                glow.registry_info["pull_thrus"][self.image.registry],
-                self.image.name,
-                self.ibw.tag)
-            return image_loc
-        else:
-            image_loc = "%s/%s" % (self.image.registry, self.image.name)
-            logging.warning("No pull through location set for registry: %s" % self.image.registry)
-            return image_loc
-
-    def docker_pull(self, command):
-        try:
-            image_pull = subprocess.check_output(command)
-        except subprocess.CalledProcessError as e:
-            msg = "Failed running command: %s\n%s" % (command, e)
-            self.data["status_reason"] = msg
-            logging.error(msg)
-            return False
-        logging.info("Successfully pulled image")
-        return image_pull
-
-    def _handle_error_pull(self) -> bool:
->>>>>>> 6960226 (working on engine-scans)
         """Handle an error pulling an image."""
         self.ibw.status = False
         self.ibw.status_ts = date_utils.json_date_now()
@@ -197,10 +152,6 @@ class ImageScan:
         else:
             self.ibw.fail_count += 1
         self.ibw.status_reason = self.data["status_reason"]
-<<<<<<< HEAD
-
-=======
->>>>>>> 6960226 (working on engine-scans)
         self.task.status_reason = self.data["status_reason"]
         self.task.end_ts = date_utils.now()
         self.task.save()
@@ -213,7 +164,6 @@ class ImageScan:
             logging.error("Could not save ibw failed status")
             return False
 
-<<<<<<< HEAD
     def _handle_success_scan(self) -> bool:
         """Handle a success scanning an image."""
         # Handle class data
@@ -227,27 +177,24 @@ class ImageScan:
         self.ib.scan_last_ts = date_utils.json_date_now()
         self.ib.save()
 
-=======
-    def _handle_success_pull(self) -> bool:
-        """Handle a success pulling an image."""
+    def _handle_success_scan(self) -> bool:
+        """Handle a success scanning an image."""
         # Handle class data
         self.data["status"] = True
-        self.data["status_reason"] = "Succeed downloading"
+        self.data["status_reason"] = "Succeed scanning"
+
         # Handle IBW
-        self.ibw.status = True
-        self.ibw.status_ts = date_utils.json_date_now()
-        self.ibw.waiting_for = "scan"
-        self.ibw.save()
+        self.ibw.delete()
+
         # Handle IB
-        self.ib.sync_last_ts = date_utils.json_date_now()
+        self.ib.scan_last_ts = date_utils.json_date_now()
         self.ib.save()
->>>>>>> 6960226 (working on engine-scans)
+
         # Handle Task
         self.task.end_ts = date_utils.now()
         self.task.save()
         return True
 
-<<<<<<< HEAD
     def _parse_scan_vulns(self, vulns: list) -> dict:
         """Parses the scan results for vulnerabilities and hydrates a dict to be used for saving
         them.
@@ -291,25 +238,5 @@ class ImageScan:
                 logging.warning("Uknown CVE severity: %s\n%s" % (cve_sev, vuln))
         return data
 
-=======
-    def _get_sha_from_docker_pull(self, image_pull: str) -> str:
-        """Get the sha from a Docker image pull command."""
-        if "sha256" not in image_pull:
-            logging.error("Cannot get sha from docker pull command.")
-            return False
-
-        tmp = image_pull[image_pull.find("sha256") + 7:]
-        tmp = tmp[:tmp.find("\n")]
-        return tmp
-
-    def _create_ib_from_pull(self, image_pull: str):
-        # sha = self._get_sha_from_docker_pull(image_pull.decode("utf-8"))
-        logging.critical("NEED TO WRITE THIS CODE!")
-        exit(1)
-        # ib_args = {
-        #     "image_id": image.id,
-        #     "sha": sha
-        # }
->>>>>>> 6960226 (working on engine-scans)
 
 # End File: cver/src/cver/engine/modules/image_scan.py
