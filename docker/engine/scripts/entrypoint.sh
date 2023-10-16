888037144adfe63aa97fc759554499d8fac92b5e
Alix Fullerton <fullertonalix0@gmail.com>
2023-10-16 11:37:54 -0400

    Adding api truncation, adding engine debug

--- docker/engine/scripts/entrypoint.sh
+++ docker/engine/scripts/entrypoint.sh
@@ -3,4 +3,6 @@
 set -e
 echo "Cver Engine Starting"
 dockerd --log-level fatal &
+echo "Alix - Docker Started"
+echo "Starting Engine"
 /usr/bin/timeout ${TIMEOUT_SECONDS} python3 /app/cver/engine/engine.py
