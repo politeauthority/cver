dd35a08256db2c3f6b11219ba5e1ba74c77d7408
Alix <fullertonalix0@gmail.com>
2023-10-18 18:32:54 -0400

    Cver Client Upgrades (#107)

--- src/cver/api/collects/base.py
+++ src/cver/api/collects/base.py
@@ -382,7 +382,7 @@ class Base:
                 sql_tools.sql_safe(where_a['field']),
                 op,
                 where_a['value'])
-            where_and_sql = where_and_sql.strip()
+            # where_and_sql = where_and_sql.strip()
 
         if where:
             where_and_sql = "WHERE " + where_and_sql
@@ -437,6 +437,8 @@ class Base:
             FROM `%s`
             ORDER BY created_ts DESC
             LIMIT %s;""" % (self.table_name, sql_tools.sql_safe(num_units))
+        print(sql)
+        return sql
 
     def _gen_get_by_ids_sql(self, model_ids: list) -> str:
         """Generate the get_by_ids SQL statement.
