b2d976fc5a85d257c9accc75c822d993163e68ea
Alix Fullerton <fullertonalix0@gmail.com>
2023-11-05 23:57:22 -0700

    Working on test users

--- src/cver/migrate/data/data_users.py
+++ src/cver/migrate/data/data_users.py
@@ -8,6 +8,7 @@ import os
 
 from cver.api.utils import glow
 from cver.api.collects.users import Users
+from cver.api.collects.roles import Roles
 from cver.api.models.api_key import ApiKey
 from cver.api.models.user import User
 from cver.api.models.organization import Organization
@@ -19,6 +20,15 @@ class DataUsers:
     def __init__(self):
         self.rbac = None
         self.org_id = None
+        self.roles = {}
+        self.get_all_roles()
+
+    def get_all_roles(self):
+        roles = Roles().get_all()
+        self.roles = {}
+        for role in roles:
+            self.roles[role.slug_name] = role
+        return True
 
     def create(self, rbac: dict) -> bool:
         """Create the first user, and test users if this is a test environment."""
@@ -26,7 +36,7 @@ class DataUsers:
         self.role_admin_id = rbac["admin_role_id"]
         self.create_first_org()
         self.create_first_user()
-        self.create_test_user()
+        self.create_test_users()
 
     def create_first_org(self) -> bool:
         """Create the first Organization
@@ -72,43 +82,50 @@ class DataUsers:
         print("\t Client ID: %s" % client_id)
         print("\t Api Key: %s" % key)
 
-    def create_test_user(self):
-        """Create test Users, with given pre-known keys."""
+    def create_test_users(self):
         if not glow.general["CVER_TEST"]:
             logging.info("Not creating test users")
-            return False
+            return True
 
-        user = User()
-        if user.get_by_email("test@example.com"):
-            logging.info("Test user already exists: %s" % user)
-            return False
-        test_client_id = os.environ.get("CVER_TEST_CLIENT_ID")
-        if not test_client_id:
-            logging.error("Missing: CVER_TEST_CLIENT_ID")
-            return False
-        test_api_key = os.environ.get("CVER_TEST_API_KEY")
-        if not test_api_key:
-            logging.error("Missing: CVER_TEST_API_KEY")
-            return False
+        # Create User: test-admin
+        client_id = os.environ.get("CVER_TEST_CLIENT_ID")
+        api_key = os.environ.get("CVER_TEST_ADMIN_API_KEY")
+        roles_id = self.roles["admin"].id
+        self.create_user("test-admin", "test-admin@example.com", roles_id, client_id, api_key)
 
+        # Create User: test-ingest
+        client_id = os.environ.get("CVER_TEST_INGEST_CLIENT_ID")
+        api_key = os.environ.get("CVER_TEST_INGEST_API_KEY")
+        roles_id = self.roles["ingestor"].id
+        self.create_user("test-ingest", "ingest@example.com", roles_id, client_id, api_key)
+
+        # # Create User: test-engine
+        # client_id = os.environ.get("CVER_TEST_ENGINE_CLIENT_ID")
+        # api_key = os.environ.get("CVER_TEST_ENGINE_API_KEY")
+        # roles_id = self.roles["engine"].id
+        # self.create_user("test-engine", "engine@example.com", roles_id, client_id, api_key)
+
+    def create_user(self, user_name, user_email, role_id, client_id, api_key) -> bool:
         user = User()
-        user.email = "test@example.com"
-        user.name = "test"
-        user.role_id = self.role_admin_id
-        user.org_id = self.org_id
-        user.save()
-        client_id = test_client_id
+        user.get_by_email(user_email)
+        if user:
+            logging.debug("Not Creating user arelady exists: %s" % user)
+            return False
+        user.name = user_name
+        user.role_id = role_id
+        user.org_id = 1
+        if not user.save():
+            logging.error("Couldnt save %s" % user)
+            return False
         api_key = ApiKey()
         api_key.user_id = user.id
         api_key.client_id = client_id
-        api_key.key = auth.generate_hash(test_api_key)
+        api_key.key = auth.generate_hash(api_key)
         api_key.save()
         print("Created")
         print("\t%s" % user)
         print("\t Client ID: %s" % client_id)
-        print("\t Api Key: %s" % test_api_key)
-
+        print("\t Api Key: %s" % api_key)
         return True
-
 
 # End File: cver/src/migrate/data/data_rbac.py
